a4bcb166b4a0a6d3f46e7eaaf00a4f231882e685
levifeyd <kuleshov-as@mail.ru>
2023-04-30 15:09:44 +0400

    rename APP_NAME

--- project/resources/views/welcome.blade.php
+++ project/resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
-                <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Библиотека</a>
+                <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Списки</a>
             @else
                 <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Войти</a>
                 @if (Route::has('register'))
