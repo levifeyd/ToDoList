a55886a8c36d808596bb869464012d16d454d2b3
levifeyd <kuleshov-as@mail.ru>
2023-04-30 15:20:48 +0400

    create model ToDoList and migration ToDoList table

--- project/app/Models/ToDoList.php
+++ project/app/Models/ToDoList.php
@@ -2,16 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tag extends Model
+class ToDoList extends Model
 {
     protected $fillable = [
         'name',
-        'item_id',
     ];
     public $timestamps = false;
-    public function getItem() {
-        return $this->belongsTo(Tag::class, 'item_id');
-    }
 }
